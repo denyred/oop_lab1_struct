8c18db6264ca031342dcc4440d916b69111b73f0
SPATARU DIONISIE <expert82822@gmail.com>
2022-09-20 02:34:37 +0300

    alpha 1.0

--- main/main/main.c
+++ main/main/main.c
@@ -11,18 +11,22 @@
 
 int main()
 {
-    printf("\n\n\t\tQueue options\n\n\n");
+    printf("\n\n\t\tLinked List options\n\n\n");
     int choice;
+    
+    FILE * fpointer = fopen("data.txt", "w");
 
     while(1)
     {
-        printf("1. Queue \n");
-        printf("2. Dequeue\n");
-        printf("3. Search\n");
-        printf("4. Sort\n");
-        printf("5. Reverse\n");
-        printf("6. Priority queue\n");
-        printf("7. Circular queue\n");
+        printf("1. Append\n");
+        printf("2. Prepend\n");
+        printf("3. Reverse\n");
+        printf("4. Add a value to the specific index \n");
+        printf("5. Remove a value the from index\n");
+        printf("6. Sort the linked list\n");
+        printf("7. Search for a value \n");
+        printf("8. Join two linked lists \n");
+        printf("9. Backwards traversal \n");
         printf("0. Exit\n\n\n");
         printf("Enter your choice :  ");
         scanf("%d",&choice);
@@ -56,8 +60,17 @@ int main()
             case 7:
                 printf("Enter number:\n");
                 break;
+                
+            case 8:
+                printf("Enter number:\n");
+                break;
+                
+            case 9:
+                printf("Enter number:\n");
+                break;
         
             case 0:
+                fclose(fpointer);
                 return(0);    // terminates the complete program execution
         }
     }
