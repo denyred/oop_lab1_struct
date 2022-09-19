e5137a698228daecebd3d6829341e2a9f53ad1b4
SPATARU DIONISIE <expert82822@gmail.com>
2022-09-19 19:11:17 +0300

    new proj

---
+++ main/main/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  main
+//
+//  Created by Spataru Dionisie on 19.09.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    printf("Hello, World!\n");
+    return 0;
+}
